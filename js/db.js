79bf2d2111d83ff111dc342f500220d5d5fc4d96
Gabriela Ordonez <gabiord9@gmail.com>
2022-11-14 00:08:39 -0300

    first commit

---
+++ js/db.js
@@ -0,0 +1,93 @@
+const Macetas = [
+    {
+    nombre:"Maceta abstracta",
+    informacion:"mediana",
+    precio: 700,
+    imagen: URL("https://i.pinimg.com/originals/f2/fc/3e/f2fc3e0ebaaba94d49a53b301e5c477b.jpg")
+    },
+
+    {
+    nombre: "Maceta Rosada",
+    informacion: "mediana",
+    precio: 900,
+    imagen: URL("https://i.pinimg.com/originals/51/eb/8f/51eb8f459146f4939ca264199bc33d8d.jpg")
+    },
+    
+    {
+    nombre: "Maceta Redonda con Pintas",
+    informacion: "chica",
+    precio: 500,
+    imagen: URL("https://i.pinimg.com/originals/f5/9a/2e/f59a2ea1e13849a5205b5cd4c6cf6064.jpg")
+    },
+
+    {
+    nombre: "Maceta pastel",
+    informacion: "mediana",
+    precio: 750,
+    imagen: URL("https://i.pinimg.com/originals/91/9f/d9/919fd9f22fcc4ccf384c8b484e6a95ba.jpg")
+    },
+
+    {
+    nombre: "Maceta Blanca Minimalista",
+    informacion: "mediana",
+    precio: 750,
+    imagen: URL("https://i.pinimg.com/originals/3d/66/2b/3d662b87e35bc4d8449cb4b5b5d11a8a.jpg")
+    },    
+
+    {
+    nombre: "Maceta Inspo Marina",
+    informacion: "mediana",
+    precio: 800,
+    imagen: URL("https://i.pinimg.com/originals/2c/12/bb/2c12bb75f6f9d33b0990b845f342f740.jpg")
+    },
+
+    {
+    nombre: "Maceta Obalos",
+    informacion: "chica",
+    precio: 400,
+    imagen: URL("https://i.pinimg.com/originals/c9/cb/70/c9cb7010f0e4ba89b0282fa9a62e0d93.jpg")
+    },               
+]
+
+const vajillas = [
+    {
+    nombre: "Bajillas de puntos",
+    informacion: "3 Piezas",
+    precio: 1300,
+    imagen: URL("https://i.pinimg.com/originals/9f/52/b2/9f52b278193595e86f504a0d1b48578e.jpg")
+    },
+
+    {
+    nombre: "Tazas para mugs",
+    informacion: "Set 2 Piezas",
+    precio: 890,
+    imagen: URL("https://i.pinimg.com/originals/ae/b6/64/aeb66476535ec7ba75b73dbf402e5d38.jpg")
+    },
+    
+    {
+    nombre: "Vasos con Oro",
+    informacion: "Set 2 Piezas",
+    precio: 1150,
+    imagen: URL("https://i.pinimg.com/originals/b5/01/04/b501042896218367b20a113c7acd8263.jpg")
+    },          
+    
+]
+
+const floreros = [
+    {
+    nombre: "Floreros rusticos, tipo cemento",
+    informacion: "3 Piezas",
+    precio: 1890,
+    imagen: URL("https://i.pinimg.com/originals/12/d7/ce/12d7ce32d6d8f51d0a78d598cf16699e.jpg")
+    },
+    
+    {
+    nombre: "Florero azul",
+    informacion: "1 Pieza",
+    precio: 980,
+    imagen: URL("https://i.pinimg.com/originals/e3/71/90/e371900b1a0f52c53599006c390977c8.jpg")
+    },      
+]
+
+
+    
